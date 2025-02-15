61ca091c7f64ac25c8371018b60f7ce7bbbc71d2
mudassir-arif <mudassir@aptechgdn.net>
2025-02-15 13:15:28 +0500

    top layout is found

--- softbanz/src/App.tsx
+++ softbanz/src/App.tsx
@@ -1,16 +1,18 @@
 import ThemeProvider from './theme/ThemeProvider';
 // import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import Login from './pages/Login/Login'
-import { Route, Routes } from 'react-router-dom';
 import PatientList from './pages/PatientList/PatientList';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
 
 const App = () => {
   return (
     <div>
       <ThemeProvider>
       <Routes>
+        <Route path="/" element={<Home />}/>
         <Route path="/login" element={<Login />}/>
-        <Route path="/" element={<PatientList />}/>
+        <Route path="/patientList" element={<PatientList />}/>
       </Routes>
       </ThemeProvider>
     </div>
